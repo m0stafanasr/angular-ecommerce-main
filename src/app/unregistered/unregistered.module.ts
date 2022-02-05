666124a94367d86c045a2a19335b674d95fc6239
mostafa nasr <mostafanasr2020@gmail.com>
2022-02-06 01:55:51 +0200

    formsmodule

--- src/app/unregistered/unregistered.module.ts
+++ src/app/unregistered/unregistered.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NewUserComponent } from './new-user/new-user.component';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../guards/auth.guard';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 const routes:Routes=[
   {path:'', redirectTo:"/user/registernewUser", pathMatch:"full"},
@@ -13,6 +14,8 @@ const routes:Routes=[
   declarations: [NewUserComponent],
   imports: [
     CommonModule,
+    FormGroup,
+    ReactiveFormsModule,
     RouterModule.forChild(routes)
   ]
 })
